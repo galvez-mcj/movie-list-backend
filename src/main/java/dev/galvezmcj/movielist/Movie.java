08ec8232c993dd26841f3acf784c587c30dbcfc5
tinay <galvez.mcj@gmail.com>
2023-04-19 14:05:54 +0800

    added class for movie and reviews

---
+++ src/main/java/dev/galvezmcj/movielist/Movie.java
@@ -0,0 +1,30 @@
+package dev.galvezmcj.movielist;
+
+import lombok.AllArgsConstructor; // create a constructor will all attributes
+import lombok.Data; // handles the getters and setters without having to write code
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+
+}
